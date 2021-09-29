ca263edf2da5eca632eb998dd6d9ae0cbb32c504
Natalia <dnataliacr@gmail.com>
2021-09-28 22:50:42 -0300

    install transformer remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "brujalquimista",
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [`gatsby-plugin-react-helmet`,
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      // Footnotes mode (default: true)
+      footnotes: true,
+      // GitHub Flavored Markdown mode (default: true)
+      gfm: true,
+      // Plugins configs
+      plugins: [],
+    },
+  },],
 };
