f4fb73572baeaf9148028ef4d721ac9937929703
Natalia <dnataliacr@gmail.com>
2021-09-29 13:53:49 -0300

    gemoterapia content

---
+++ src/pages/gemoterapia.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Layout from "../components/layout/Layout";
+
+import { Helmet } from "react-helmet";
+
+const GemoterapiaPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Brujalquimista | Gemoterapia</title>>
+      </Helmet>
+      <Layout />
+    </>
+  );
+};
+
+export default GemoterapiaPage;
