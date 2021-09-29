f8906452a226dea6fbf03521cbfe40891ff87ca9
Natalia <dnataliacr@gmail.com>
2021-09-29 13:54:53 -0300

    reflexologia content

---
+++ src/pages/reflexologia.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Layout from "../components/layout/Layout";
+
+import { Helmet } from "react-helmet";
+
+const ReflexologiaPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Brujalquimista | Reflexología </title>
+      </Helmet>
+      <Layout />
+    </>
+  );
+};
+
+export default ReflexologiaPage;
