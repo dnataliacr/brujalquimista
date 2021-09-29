1af993caabf1d599fb0767d1e7c3cdc811d77978
Natalia <dnataliacr@gmail.com>
2021-09-29 18:10:05 -0300

    add therapy content

---
+++ src/components/therapy/Therapy.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./terapy.css";
+
+const Therapy = ({ content }) => {
+  return (
+    <div className="therapy-body">
+       <div className="therapy-img" />
+      <div className="therapy-container">
+       
+
+        <h1>{content.frontmatter.title}</h1>
+      
+        <div
+          dangerouslySetInnerHTML={{ __html: content.html }}
+          className="therapy-text"
+        />
+          <hr/>
+      </div>
+    </div>
+  );
+};
+
+export default Therapy;
