7436f3253bf218d94fb43b61ca4d78fa9e1765d4
Natalia <dnataliacr@gmail.com>
2021-09-29 14:06:34 -0300

    add Home elements

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Hero from "./hero/Hero";
+import Therapies from "./therapies/Therapies";
+import Form from "./contact/Form";
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Therapies />
+    </>
+  );
+};
+
+export default Home;
