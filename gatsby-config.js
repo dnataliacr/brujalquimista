0e220efb0a75b990eab9d7d89371add7fe6bfd27
Natalia <dnataliacr@gmail.com>
2021-09-29 18:31:02 -0300

    just delete a comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       options: {
         name: `data`,
         path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
+        ignore: [`**/\.*`], 
       },
     },
   ],
