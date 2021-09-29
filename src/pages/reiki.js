f3033db449314c5c5fb221bbceb83ba001336aab
Natalia <dnataliacr@gmail.com>
2021-09-29 13:52:58 -0300

    create reiki page

---
+++ src/pages/reiki.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Layout from "../components/layout/Layout";
+
+import { Helmet } from "react-helmet";
+
+const ReikiPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Brujalquimista | Reiki </title>
+      </Helmet>
+      <Layout />
+    </>
+  );
+};
+
+export default ReikiPage;
