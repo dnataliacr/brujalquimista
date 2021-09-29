a5be8e70a5d9ed088dd2092176fa410d1ffb2bd9
Natalia <dnataliacr@gmail.com>
2021-09-29 13:58:09 -0300

    Create contacto.js

---
+++ src/pages/contacto.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Layout from "../components/layout/Layout";
+
+import { Helmet } from "react-helmet";
+
+const ContactoPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Brujalquimista | Contacto</title>
+      </Helmet>
+      <Layout />
+    </>
+  );
+};
+
+export default ContactoPage;
