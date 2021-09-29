c15dfffd0e747d2d34d8a262381c4921c91bbc89
Natalia <dnataliacr@gmail.com>
2021-09-29 14:06:10 -0300

    add hero content

---
+++ src/components/home/hero/Hero.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import video from "../../../assets/video.mp4";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="hero-body">
+        <div className="hero-video-container">
+          <video src={video} type="video/mp4" autoPlay loop muted playsInline />
+        </div>
+        <div className="hero-content">
+          <div className="hero-items">
+            <h1>Despertá la magia que hay en vos</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
