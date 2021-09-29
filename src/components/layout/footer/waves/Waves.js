15b0eddda5ae9f7fce8ea877d866c36f2842cb30
Natalia <dnataliacr@gmail.com>
2021-09-29 13:35:58 -0300

    add waves component

---
+++ src/components/layout/footer/waves/Waves.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./waves.css";
+
+const Waves = () => {
+  return (
+    <>
+      <section className="shapedividers_com-6052"></section>
+    </>
+  );
+};
+
+export default Waves;
