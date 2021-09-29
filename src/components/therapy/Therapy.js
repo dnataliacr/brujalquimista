80be1febfe3c19e9d6defcc367ea16030246e429
Natalia <dnataliacr@gmail.com>
2021-09-29 18:15:12 -0300

    Update Therapy.js

--- src/components/therapy/Therapy.js
+++ src/components/therapy/Therapy.js
@@ -4,17 +4,15 @@ import "./terapy.css";
 const Therapy = ({ content }) => {
   return (
     <div className="therapy-body">
-       <div className="therapy-img" />
+      <div className="therapy-img" />
       <div className="therapy-container">
-       
-
         <h1>{content.frontmatter.title}</h1>
-      
+
         <div
           dangerouslySetInnerHTML={{ __html: content.html }}
           className="therapy-text"
         />
-          <hr/>
+        <hr />
       </div>
     </div>
   );
