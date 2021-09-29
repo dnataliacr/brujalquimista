20354c6c4a19c622efb62ae155e7d1dee50f83f6
Natalia <dnataliacr@gmail.com>
2021-09-29 18:20:28 -0300

    I forgot install plugin source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,34 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "brujalquimista",
   },
-  plugins: [`gatsby-plugin-react-helmet`,
-  `gatsby-plugin-styled-components`,
-  {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      // Footnotes mode (default: true)
-      footnotes: true,
-      // GitHub Flavored Markdown mode (default: true)
-      gfm: true,
-      // Plugins configs
-      plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
     },
-  },],
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+  ],
 };
