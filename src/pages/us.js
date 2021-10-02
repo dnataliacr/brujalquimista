60403ddb4adc2be094f89657639573d5752d2ab7
Natalia <dnataliacr@gmail.com>
2021-10-02 19:52:17 -0300

    create us page

---
+++ src/pages/us.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+
+import Layout from "../components/layout/Layout";
+
+// markup
+const UsPage = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Brujalquimista | Nosotras</title>
+      </Helmet>
+      <Layout>
+  
+      </Layout>
+    </>
+  );
+};
+
+export default IndexPage;
